763e218330c475768b34a077c6c3501989fee79f
Maxine Cruz <terese.cruz@gmail.com>
2023-04-30 21:33:20 -0700

    Push files

---
+++ src/NAm_data_further_reduced.R
@@ -0,0 +1,22 @@
+# Maxine Cruz
+# tmcruz@arizona.edu
+# Created: 30 April 2023
+# Last modified: 30 April 2023
+
+
+
+### ABOUT THE SCRIPT ---
+
+# Further filters data per Dr. Buchanan's request (see other_files folder)
+
+# Reduces data to:
+  # Centris pallida - 1342915
+  # Olneya tesota - 2974564
+  # Parkinsonia florida - 5359949
+  # Parkinsonia microphylla - 5359945
+
+# And removes unlikely observations
+
+
+
+### 
