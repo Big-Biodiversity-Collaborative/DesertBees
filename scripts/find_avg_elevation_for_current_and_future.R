f0a298dfa78f8b755e2345e613a8fb33d80d0930
Maxine Cruz <terese.cruz@gmail.com>
2024-05-14 13:59:33 -0700

    Forgot to push these distribution map updates + script for finding average elevation in current / future distribution

---
+++ scripts/find_avg_elevation_for_current_and_future.R
@@ -0,0 +1,178 @@
+# Maxine Cruz
+# tmcruz@arizona.edu
+# Created: 14 May 2024
+# Last modified: 14 May 2024
+
+
+
+
+# ----- ABOUT -----
+
+# Finds average elevation for current and future distributions
+  # Test / template script containing comments is at the bottom
+
+
+
+
+# ----- LOAD LIBRARIES -----
+
+library(dplyr)
+library(terra)
+
+
+
+
+# ----- LOAD DATA -----
+
+# Species occurrence data
+data <- read.csv("data/gbif/cleaned_species_with_elevation.csv")
+
+# Elevation data
+dem <- rast("data/dem/northamerica_elevation_cec_2023.tif")
+
+# Climate data
+clim <- terra::rast(list.files(path = "data/worldclim",
+                               pattern = ".tif$",
+                               full.names = TRUE))
+
+# Change projection of dem to that of bioclim variables (may take a minute)
+dem_mod <- terra::project(dem, crs(clim))
+
+
+
+
+# ----- FIND AVERAGE ELEVATION ACROSS CURRENT AND FUTURE DISTRIBUTIONS -----
+
+# Species to loop through
+spp_list <- unique(data$species)
+
+# Empty table
+averages <- data.frame()
+
+# Loop for finding averages and adding them to a table
+for (i in 1:4) {
+  
+  # i) Access file names
+  file_id <- gsub(" ", "_", tolower(spp_list[i]))
+  
+  # ii) Current distribution
+  current <- read.csv(paste0("output/", file_id, "/worldclim_predicted_distribution_adjusted.csv"))
+  
+  coords <- current %>%
+    select(x, y)
+  
+  spp <- SpatialPoints(coords,
+                       proj4string = CRS("+proj=longlat"))
+  
+  occs <- vect(spp)
+  
+  elevocc1 <- terra::extract(dem_mod, occs, ID = TRUE) %>%
+    cbind(geom(occs))
+  
+  elevocc1 <- elevocc1 %>%
+    select(5, 6, 2) %>%
+    rename(latitude = y,
+           longitude = x,
+           elevation = northamerica_elevation_cec_2023)
+  
+  # iii) Future distribution
+  future <- read.csv(paste0("output/", file_id, "/ssp245_2021_predicted_distribution_adjusted.csv"))
+  
+  coords <- future %>%
+    select(x, y)
+  
+  spp <- SpatialPoints(coords,
+                       proj4string = CRS("+proj=longlat"))
+  
+  occs <- vect(spp)
+  
+  elevocc2 <- terra::extract(dem_mod, occs, ID = TRUE) %>%
+    cbind(geom(occs))
+  
+  elevocc2 <- elevocc2 %>%
+    select(5, 6, 2) %>%
+    rename(latitude = y,
+           longitude = x,
+           elevation = northamerica_elevation_cec_2023)
+  
+  # iv) Calculate averages for this species and append to full data frame
+  new_df <- data.frame(species = file_id,
+                       current = mean(elevocc1$elevation),
+                       future = mean(elevocc2$elevation))
+  
+  averages <- rbind(averages, new_df)
+  
+}
+
+# End of main loop
+
+# Save table
+write.csv(averages,
+          file = "output/average_elevations.csv",
+          row.names = FALSE)
+
+
+
+
+# ----- TESTING / TEMPLATE -----
+
+# a) C. pallida ---
+
+# i) Current distribution
+current <- read.csv("output/centris_pallida/worldclim_predicted_distribution_adjusted.csv")
+
+# Isolate lat/long
+coords <- current %>%
+  select(x, y)
+
+# Convert to spatial points
+spp <- SpatialPoints(coords,
+                     proj4string = CRS("+proj=longlat"))
+
+# Convert to SpatVector
+occs <- vect(spp)
+
+# Extract elevation values for occurrence points
+elevocc <- terra::extract(dem_mod, occs, ID = TRUE) %>%
+  cbind(geom(occs))
+
+# Isolate lat/long and elevation
+elevocc <- elevocc %>%
+  select(5, 6, 2) %>%
+  rename(latitude = y,
+         longitude = x,
+         elevation = northamerica_elevation_cec_2023)
+
+# Average elevation
+message(paste0("Average elevation for current distribution of C. pallida: "),
+        mean(elevocc$elevation))
+
+# ii) Future distribution
+future <- read.csv("output/centris_pallida/ssp245_2021_predicted_distribution_adjusted.csv")
+
+# Isolate lat/long
+coords <- future %>%
+  select(x, y)
+
+# Convert to spatial points
+spp <- SpatialPoints(coords,
+                     proj4string = CRS("+proj=longlat"))
+
+# Convert to SpatVector
+occs <- vect(spp)
+
+# Extract elevation values for occurrence points
+elevocc <- terra::extract(dem_mod, occs, ID = TRUE) %>%
+  cbind(geom(occs))
+
+# Isolate lat/long and elevation
+elevocc <- elevocc %>%
+  select(5, 6, 2) %>%
+  rename(latitude = y,
+         longitude = x,
+         elevation = northamerica_elevation_cec_2023)
+
+# Average elevation
+message(paste0("Average elevation for future distribution of C. pallida: "),
+        mean(elevocc$elevation))
+
--- scripts/species_distribution_maps.R
+++ scripts/species_distribution_maps.R
@@ -1,7 +1,7 @@
 # Maxine Cruz
 # tmcruz@arizona.edu
 # Created: 22 March 2024
-# Last modified: 22 April 2024
+# Last modified: 14 May 2024
 
 
 
@@ -9,10 +9,8 @@
 # ----- ABOUT -----
 
 # Generates ggplot maps from Maxent results
-
-# 1) Creates strips of 2000-2021, 2021-40, and 2041-60 predictions for each species
-# 2) Arranges species strips so the final plot is a panel of scenarios
-
+  # 1) Creates a grid of current distributions (4 panels)
+  # 2) Creates a grid of future distributions (4 panels)
 
 
 
@@ -42,6 +40,162 @@ source("functions.R")
 # Collect data for map borderlines
 wrld <- ggplot2::map_data("world")
 
+# Major cities
+city_coords <- data.frame(city = c("Phoenix", "Tucson", "Las Vegas", 
+                                   "Los Angeles", "San Diego", "Hermosillo"),
+                          latitude = c(33.6055497, 32.1560939, 36.1251645,
+                                       34.0206085, 32.8246976, 29.0834732),
+                          longitude = c(-112.4547019, -111.0486154, -115.3398097,
+                                        -118.7413831, -117.4386299, -111.0707154))
+
+
+
+
+# ----- CURRENT MAPS -----
+
+# P. florida seems to have the widest area so we'll match the axes for all maps
+  # to that one. This also makes it so plot_grid doesn't come out wonky.
+
+# P. florida zoom box
+zoom_data <- read.csv("output/parkinsonia_florida/worldclim_predicted_distribution_adjusted.csv")
+
+xmax <- max(zoom_data$x) + 1
+xmin <- min(zoom_data$x) - 1
+ymax <- max(zoom_data$y) + 1
+ymin <- min(zoom_data$y) - 1
+
+# Now all maps should have the same axes
+
+# C. pallida
+
+data <- read.csv("output/centris_pallida/worldclim_predicted_distribution_adjusted.csv")
+plot_a <- custom_ggplot1(sdm_data = data, 
+                         xmin = xmin, xmax = xmax, 
+                         ymin = ymin, ymax = ymax)
+
+# O. tesota
+
+data <- read.csv("output/olneya_tesota/worldclim_predicted_distribution_adjusted.csv")
+plot_b <- custom_ggplot1(sdm_data = data, 
+                         xmin = xmin, xmax = xmax, 
+                         ymin = ymin, ymax = ymax)
+
+# P. florida
+
+data <- read.csv("output/parkinsonia_florida/worldclim_predicted_distribution_adjusted.csv")
+plot_c <- custom_ggplot1(sdm_data = data, 
+                         xmin = xmin, xmax = xmax, 
+                         ymin = ymin, ymax = ymax)
+
+# P. microphylla
+
+data <- read.csv("output/parkinsonia_microphylla/worldclim_predicted_distribution_adjusted.csv")
+plot_d <- custom_ggplot1(sdm_data = data, 
+                         xmin = xmin, xmax = xmax, 
+                         ymin = ymin, ymax = ymax)
+
+# Plot as a grid
+current_grid <- plot_grid(plot_a + theme(legend.position = "none"), 
+                          plot_b + theme(legend.position = "none"), 
+                          plot_c + theme(legend.position = "none"), 
+                          plot_d + theme(legend.position = "none"),
+                          labels = "auto",
+                          label_size = 20,
+                          ncol = 2)
+
+# Get legend from one plot to use for all plots
+legend <- get_legend(plot_a)
+
+# Plot grid with legend
+current_grid2 <- plot_grid(current_grid, legend, rel_widths = c(3, 0.6))
+
+# Check grid
+current_grid2
+
+# Save plot
+ggsave("output/distribution_maps/species_distribution_maps_current.png", 
+       current_grid2,
+       width = 17.33,
+       height = 17.445,
+       units = "cm")
+
+
+
+
+# ----- FUTURE MAPS -----
+
+# C. pallida seems to have the widest area so we'll match the axes for all maps
+  # to that one. This also makes it so plot_grid doesn't come out wonky.
+
+# C. pallida zoom box
+zoom_data <- read.csv("output/centris_pallida/ssp245_2021_predicted_distribution_adjusted.csv")
+
+xmax <- max(zoom_data$x) + 1
+xmin <- min(zoom_data$x) - 1
+ymax <- max(zoom_data$y) + 1
+ymin <- min(zoom_data$y) - 1
+
+# Now all maps should have the same axes
+
+# C. pallida
+
+data <- read.csv("output/centris_pallida/ssp245_2021_predicted_distribution_adjusted.csv")
+plot_a <- custom_ggplot2(sdm_data = data, 
+                         xmin = xmin, xmax = xmax, 
+                         ymin = ymin, ymax = ymax)
+
+# O. tesota
+
+data <- read.csv("output/olneya_tesota/ssp245_2021_predicted_distribution_adjusted.csv")
+plot_b <- custom_ggplot2(sdm_data = data, 
+                         xmin = xmin, xmax = xmax, 
+                         ymin = ymin, ymax = ymax)
+
+# P. florida
+
+data <- read.csv("output/parkinsonia_florida/ssp245_2021_predicted_distribution_adjusted.csv")
+plot_c <- custom_ggplot2(sdm_data = data, 
+                         xmin = xmin, xmax = xmax, 
+                         ymin = ymin, ymax = ymax)
+
+# P. microphylla
+
+data <- read.csv("output/parkinsonia_microphylla/ssp245_2021_predicted_distribution_adjusted.csv")
+plot_d <- custom_ggplot2(sdm_data = data, 
+                         xmin = xmin, xmax = xmax, 
+                         ymin = ymin, ymax = ymax)
+
+# Plot as a grid
+current_grid <- plot_grid(plot_a + theme(legend.position = "none"), 
+                          plot_b + theme(legend.position = "none"), 
+                          plot_c + theme(legend.position = "none"), 
+                          plot_d + theme(legend.position = "none"),
+                          labels = "auto",
+                          label_size = 20,
+                          ncol = 2)
+
+# Get legend from one plot to use for all plots
+legend <- get_legend(plot_a)
+
+# Plot grid with legend
+current_grid2 <- plot_grid(current_grid, legend, rel_widths = c(3, 0.6))
+
+# Check grid
+current_grid2
+
+# Save plot
+ggsave("output/distribution_maps/species_distribution_maps_future.png", 
+       current_grid2,
+       width = 18,
+       height = 14.779,
+       units = "cm")
+
+
+
+
+
+
+
 
 
 
@@ -55,15 +209,15 @@ wrld <- ggplot2::map_data("world")
 
 # Current
 data <- read.csv("output/centris_pallida/worldclim_predicted_distribution.csv")
-plot_a <- custom_ggplot(sdm_data = data, sdm_type = "2000-2021")
+plot_a <- custom_ggplot1(sdm_data = data, sdm_type = "2000-2021")
 
 # 2021
-data <- read.csv("output/centris_pallida/ssp370_2021_predicted_distribution.csv")
-plot_b <- custom_ggplot(sdm_data = data, sdm_type = "2021-2040")
+data <- read.csv("output/centris_pallida/ssp245_2021_predicted_distribution.csv")
+plot_b <- custom_ggplot1(sdm_data = data, sdm_type = "SSP2-4.5 2021-2040")
 
 # 2041
-data <- read.csv("output/centris_pallida/ssp370_2041_predicted_distribution.csv")
-plot_c <- custom_ggplot(sdm_data = data, sdm_type = "2041-2060")
+data <- read.csv("output/centris_pallida/ssp245_2041_predicted_distribution.csv")
+plot_c <- custom_ggplot1(sdm_data = data, sdm_type = "SSP2-4.5 2041-2060")
 
 # Strip
 cp_strip <- plot_grid(plot_a, plot_b, plot_c,
@@ -88,12 +242,12 @@ data <- read.csv("output/olneya_tesota/worldclim_predicted_distribution.csv")
 plot_a <- custom_ggplot(sdm_data = data, sdm_type = "2000-2021")
 
 # 2021
-data <- read.csv("output/olneya_tesota/ssp370_2021_predicted_distribution.csv")
-plot_b <- custom_ggplot(sdm_data = data, sdm_type = "2021-2040")
+data <- read.csv("output/olneya_tesota/ssp245_2021_predicted_distribution.csv")
+plot_b <- custom_ggplot1(sdm_data = data, sdm_type = "SSP2-4.5 2021-2040")
 
 # 2041
-data <- read.csv("output/olneya_tesota/ssp370_2041_predicted_distribution.csv")
-plot_c <- custom_ggplot(sdm_data = data, sdm_type = "2041-2060")
+data <- read.csv("output/olneya_tesota/ssp245_2041_predicted_distribution.csv")
+plot_c <- custom_ggplot1(sdm_data = data, sdm_type = "SSP2-4.5 2041-2060")
 
 # Strip
 ot_strip <- plot_grid(plot_a, plot_b, plot_c,
@@ -118,12 +272,12 @@ data <- read.csv("output/parkinsonia_florida/worldclim_predicted_distribution.cs
 plot_a <- custom_ggplot(sdm_data = data, sdm_type = "2000-2021")
 
 # 2021
-data <- read.csv("output/parkinsonia_florida/ssp370_2021_predicted_distribution.csv")
-plot_b <- custom_ggplot(sdm_data = data, sdm_type = "2021-2040")
+data <- read.csv("output/parkinsonia_florida/ssp245_2021_predicted_distribution.csv")
+plot_b <- custom_ggplot1(sdm_data = data, sdm_type = "SSP2-4.5 2021-2040")
 
 # 2041
-data <- read.csv("output/parkinsonia_florida/ssp370_2041_predicted_distribution.csv")
-plot_c <- custom_ggplot(sdm_data = data, sdm_type = "2041-2060")
+data <- read.csv("output/parkinsonia_florida/ssp245_2041_predicted_distribution.csv")
+plot_c <- custom_ggplot1(sdm_data = data, sdm_type = "SSP2-4.5 2041-2060")
 
 # Strip
 pf_strip <- plot_grid(plot_a, plot_b, plot_c,
@@ -148,12 +302,12 @@ data <- read.csv("output/parkinsonia_microphylla/worldclim_predicted_distributio
 plot_a <- custom_ggplot(sdm_data = data, sdm_type = "2000-2021")
 
 # 2021
-data <- read.csv("output/parkinsonia_microphylla/ssp370_2021_predicted_distribution.csv")
-plot_b <- custom_ggplot(sdm_data = data, sdm_type = "2021-2040")
+data <- read.csv("output/parkinsonia_microphylla/ssp245_2021_predicted_distribution.csv")
+plot_b <- custom_ggplot1(sdm_data = data, sdm_type = "SSP2-4.5 2021-2040")
 
 # 2041
-data <- read.csv("output/parkinsonia_microphylla/ssp370_2041_predicted_distribution.csv")
-plot_c <- custom_ggplot(sdm_data = data, sdm_type = "2041-2060")
+data <- read.csv("output/parkinsonia_microphylla/ssp245_2041_predicted_distribution.csv")
+plot_c <- custom_ggplot1(sdm_data = data, sdm_type = "SSP2-4.5 2041-2060")
 
 # Strip
 pm_strip <- plot_grid(plot_a, plot_b, plot_c,
@@ -208,57 +362,57 @@ ggsave("output/species_distribution_maps.png",
 
 # Current
 data <- read.csv("output/centris_pallida/worldclim_predicted_distribution.csv")
-plot_a <- custom_ggplot(sdm_data = data, sdm_type = "2000-2021")
+plot_a <- custom_ggplot1(sdm_data = data, sdm_type = "2000-2021")
 
 # 2021
-data <- read.csv("output/centris_pallida/ssp370_2021_predicted_distribution.csv")
-plot_b <- custom_ggplot(sdm_data = data, sdm_type = "2021-2040")
+data <- read.csv("output/centris_pallida/ssp245_2021_predicted_distribution.csv")
+plot_b <- custom_ggplot2(sdm_data = data, sdm_type = "SSP2-4.5 2021-2040")
 
 # 2041
-data <- read.csv("output/centris_pallida/ssp370_2041_predicted_distribution.csv")
-plot_c <- custom_ggplot(sdm_data = data, sdm_type = "2041-2060")
+data <- read.csv("output/centris_pallida/ssp245_2041_predicted_distribution.csv")
+plot_c <- custom_ggplot2(sdm_data = data, sdm_type = "SSP2-4.5 2041-2060")
 
 # -- Olneya tesota --
 
 # Current
 data <- read.csv("output/olneya_tesota/worldclim_predicted_distribution.csv")
-plot_d <- custom_ggplot(sdm_data = data, sdm_type = "2000-2021")
+plot_d <- custom_ggplot1(sdm_data = data, sdm_type = "2000-2021")
 
 # 2021
-data <- read.csv("output/olneya_tesota/ssp370_2021_predicted_distribution.csv")
-plot_e <- custom_ggplot(sdm_data = data, sdm_type = "2021-2040")
+data <- read.csv("output/olneya_tesota/ssp245_2021_predicted_distribution.csv")
+plot_e <- custom_ggplot2(sdm_data = data, sdm_type = "SSP2-4.5 2021-2040")
 
 # 2041
-data <- read.csv("output/olneya_tesota/ssp370_2041_predicted_distribution.csv")
-plot_f <- custom_ggplot(sdm_data = data, sdm_type = "2041-2060")
+data <- read.csv("output/olneya_tesota/ssp245_2041_predicted_distribution.csv")
+plot_f <- custom_ggplot2(sdm_data = data, sdm_type = "SSP2-4.5 2041-2060")
 
 # -- Parkinsonia florida --
 
 # Current
 data <- read.csv("output/parkinsonia_florida/worldclim_predicted_distribution.csv")
-plot_g <- custom_ggplot(sdm_data = data, sdm_type = "2000-2021")
+plot_g <- custom_ggplot1(sdm_data = data, sdm_type = "2000-2021")
 
 # 2021
-data <- read.csv("output/parkinsonia_florida/ssp370_2021_predicted_distribution.csv")
-plot_h <- custom_ggplot(sdm_data = data, sdm_type = "2021-2040")
+data <- read.csv("output/parkinsonia_florida/ssp245_2021_predicted_distribution.csv")
+plot_h <- custom_ggplot2(sdm_data = data, sdm_type = "SSP2-4.5 2021-2040")
 
 # 2041
-data <- read.csv("output/parkinsonia_florida/ssp370_2041_predicted_distribution.csv")
-plot_i <- custom_ggplot(sdm_data = data, sdm_type = "2041-2060")
+data <- read.csv("output/parkinsonia_florida/ssp245_2041_predicted_distribution.csv")
+plot_i <- custom_ggplot2(sdm_data = data, sdm_type = "SSP2-4.5 2041-2060")
 
 # -- Parkinsonia microphylla --
 
 # Current
 data <- read.csv("output/parkinsonia_microphylla/worldclim_predicted_distribution.csv")
-plot_j <- custom_ggplot(sdm_data = data, sdm_type = "2000-2021")
+plot_j <- custom_ggplot1(sdm_data = data, sdm_type = "2000-2021")
 
 # 2021
-data <- read.csv("output/parkinsonia_microphylla/ssp370_2021_predicted_distribution.csv")
-plot_k <- custom_ggplot(sdm_data = data, sdm_type = "2021-2040")
+data <- read.csv("output/parkinsonia_microphylla/ssp245_2021_predicted_distribution.csv")
+plot_k <- custom_ggplot2(sdm_data = data, sdm_type = "SSP2-4.5 2021-2040")
 
 # 2041
-data <- read.csv("output/parkinsonia_microphylla/ssp370_2041_predicted_distribution.csv")
-plot_l <- custom_ggplot(sdm_data = data, sdm_type = "2041-2060")
+data <- read.csv("output/parkinsonia_microphylla/ssp245_2041_predicted_distribution.csv")
+plot_l <- custom_ggplot2(sdm_data = data, sdm_type = "SSP2-4.5 2041-2060")
 
 # Full panel
 panel <- plot_grid(plot_a, plot_b, plot_c,
@@ -276,7 +430,7 @@ panel
 # Save plot
 ggsave("output/species_distribution_maps.png", 
        panel,
-       width = 30,
+       width = 25.2,
        height = 55,
        units = "cm")
 
